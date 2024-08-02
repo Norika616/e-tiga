c15092ae455c7bb2854af05256126f35498e9ae4
Norika Kikuchi <0616nori99@gmail.com>
2024-08-02 18:22:48 +0900

    add

--- js/script.js
+++ js/script.js
@@ -1,12 +1,48 @@
-$(document).ready(function () {
-  $(".profile__nav-item").click(function () {
-    // クリックされたタブのdata-target属性の値を取得
-    const targetId = $(this).data("target");
+// swiper
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  direction: "vertical",
+  loop: true,
 
-    // クリックされたタブに対応するコンテンツを表示し、activeクラスを追加
-    $(`#${targetId}`).show().addClass("active");
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
 
-    // クリックされたタブにactiveクラスを追加
-    $(this).addClass("active");
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+});
+
+const tabs = document.querySelectorAll(
+  ".company__tab--list .company__tabs .company__tab"
+);
+
+tabs.forEach((tab) => {
+  tab.addEventListener("click", () => {
+    const currentTab = document.querySelector(
+      ".company__tab--list .company__tab.active"
+    );
+    const currentContent = document.querySelector(".tab__content.active");
+
+    currentTab.classList.remove("active");
+    currentContent.classList.remove("active");
+
+    tab.classList.add("active");
+    const contentId = tab.dataset.tab;
+    const content = document.getElementById(contentId);
+
+    if (content) {
+      content.classList.add("active");
+    } else {
+      console.error(`要素が見つかりません: ${contentId}`);
+    }
   });
 });
